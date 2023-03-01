bb88cd7b5ffa8c80bc8bd1fb33287f47a60d0b15
Your <aynug21@gmail.com>
2023-03-01 01:16:14 -0800

    a function that reverses the content of an array of integers.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reverse_array - function that reverses the content of an array of integers
- * @a: an array of integer
+ * @a: an array of integers
  * @n: number of elements to swap
  * Return: nothing
  */
