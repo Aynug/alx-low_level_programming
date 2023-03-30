a2d5f7263ed3dbc1d14f875782a3355097d45627
Your <aynug21@gmail.com>
2023-03-30 09:17:40 -0700

    List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,21 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - Gets the length of a linked list
- * @h: Pointer to the first node in the list
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- * Return: size_t length of the list
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	size_t len = 0;
-	const list_t *item;
+ size_t n = 0;
 
-	for (item = h; item != NULL; item = item->next)
-		len++;
-
-	return (len);
+ while (h)
+ {
+ n++;
+ h = h->next;
+ }
+ return (n);
 }
+
