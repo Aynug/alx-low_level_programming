abfb252095312f8ddf590e775953c4dfcc36d589
Aynug <aynug21@gmail.com>
2023-06-15 13:01:18 -0700

    0x17-doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int i;
+
+	current = head;
+	i = 0;
+	while (current != NULL)
+	{
+		if (i == index)
+			return (current);
+		current = current->next;
+		i++;
+	}
+	return (NULL);
+}
