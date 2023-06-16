d77054e39b819b1cbe8555e0c51802ea7f08ec21
Aynug <aynug21@gmail.com>
2023-06-16 09:24:30 -0700

    updated

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: double pointer to the head of the list
+ * @n: data to be added
+ *
+ * Return: he address of the new element, or NULL if it failed
+ */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ *  sum_dlistint - returns the sum of all
+ * the data (n) of a dlistint_t linked list.
+ * @head: double pointer to the head of the list
+ *
+ * Return: if the list is empty, return 0
+ */
+
 int sum_dlistint(dlistint_t *head)
 {
 
